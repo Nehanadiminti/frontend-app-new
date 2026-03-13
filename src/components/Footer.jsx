b418b64c83300cec00c18a290ab0e2c3496e247d
Nehanadiminti <nehanadiminti5@gmail.com>
2026-03-13 22:33:17 +0530

    updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,31 @@
-import "./Footer.css"
-function Footer(){
-    return <h4 className="App-Footer">Copyright 2026. All rights reserved.</h4>
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+
+      <div className="footer-content">
+
+        <h3>☕ Bakingo</h3>
+
+        <p>
+          Freshly brewed coffee and delicious treats made with love.
+        </p>
+
+        <div className="footer-links">
+          <a href="/">Home</a>
+          <a href="/cart">Cart</a>
+          <a href="/orders">Orders</a>
+        </div>
+
+        <p className="copyright">
+          © 2026 Bakingo Café. All rights reserved.
+        </p>
+
+      </div>
+
+    </footer>
+  );
 }
-export default Footer
+
+export default Footer;
